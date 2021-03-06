c79f703e872a88472748ce5c1e9bcd556bc5c143
berkayyildirim <berkay.yildirim@yahoo.com>
2021-03-06 18:14:26 +0300

    implement updating logs

--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -2,11 +2,11 @@ import React from 'react';
 import Moment from "react-moment";
 import {connect} from "react-redux";
 import PropTypes from "prop-types";
-import {deleteLog} from "../../actions/logActions";
+import {deleteLog, setCurrent} from "../../actions/logActions";
 
 import M from "materialize-css/dist/js/materialize.min.js";
 
-const LogItem = ({log, deleteLog}) => {
+const LogItem = ({log, deleteLog, setCurrent}) => {
   const onDelete = () => {
     deleteLog(log.id);
     M.toast({html: "Log Deleted"});
@@ -15,7 +15,9 @@ const LogItem = ({log, deleteLog}) => {
     return (
         <li className="collection-item">
           <div>
-            <a href="#edit-log-modal" className={`modal-trigger ${log.attention ? "red-text" : "blue-text"}`}>
+            <a href="#edit-log-modal" className={`modal-trigger ${log.attention ? "red-text" : "blue-text"}`}
+            onClick={() => setCurrent(log)}
+            >
               {log.message} 
             </a>
             <br/>
@@ -34,7 +36,8 @@ const LogItem = ({log, deleteLog}) => {
 
 LogItem.propTypes = {
     log: PropTypes.object.isRequired,
-    deleteLog: PropTypes.func.isRequired
+    deleteLog: PropTypes.func.isRequired,
+    setCurrent: PropTypes.func.isRequired
 }
 
-export default connect(null, {deleteLog})(LogItem);
+export default connect(null, {deleteLog, setCurrent})(LogItem);
